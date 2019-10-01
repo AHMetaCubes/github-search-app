35472a911580d005de8d4d21b0e00cf37467095b
The React Guys <thereactguys@Thes-MacBook-Pro.local>
2019-10-01 15:59:44 -0400

    adds build to host at oppsapp.com

---
+++ build/precache-manifest.8c361eddb5620d7eb7ea0796936b530a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6319bed197c06e599930c13484a1cb03",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bdd06077b1417af6acf8",
+    "url": "/static/css/main.377bccd3.chunk.css"
+  },
+  {
+    "revision": "2c8e692af852188ce061",
+    "url": "/static/js/2.bca07562.chunk.js"
+  },
+  {
+    "revision": "bdd06077b1417af6acf8",
+    "url": "/static/js/main.b62acfa5.chunk.js"
+  },
+  {
+    "revision": "f1a21297bbc4bc510c07",
+    "url": "/static/js/runtime-main.25cb0410.js"
+  }
+]);
